3c1f5e50da6161ef66003eaf8129abf7adfe8ac6
Candy <candymdeoleo@gmail.com>
2021-12-16 14:13:47 +0100

    first commit

---
+++ src/main/java/org/factoriaf5/libritos/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package org.factoriaf5.libritos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+        public String home() {
+            return "home";
+        }
+
+}
